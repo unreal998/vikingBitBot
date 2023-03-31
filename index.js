6bcfdd71c4a8259fc1756716ca7f590f739a2d66
tivasyuk <tanyaiv494@gmail.com>
2023-03-31 21:14:21 +0300

    update

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 import BotAPI from 'node-telegram-bot-api';
 import fetch from "node-fetch";
-import { 
-    MARKET_NAMES, 
-    COUNTRIES_NAMES, 
-    SERVER_URL, 
-    REQUEST_UI_CONTROL_EVENTS, 
-    MAIN_MENU_UI_CONTROLS_EVENT, 
-    TOKEN,
-    NEW_USER_REPLY,
-    ADMINS_CHOOSE,
-    SEQUE_SCREEN_OPTIONS,
-    SETTINGS_EVENTS,
-    USER_ROLES,
-    BOT_TEAM_NAME
+import {
+    MAIN_MENU_UI_CONTROLS_EVENT,
+    SERVER_URL, SET_CURRENCY_EVENT,
+    TOKEN
 } from './src/constants.js';
 import UIManager from './src/UIManager.js';
-import OLXFlowController from './src/markets/olx/olxFlowController.js';
-import WillhabenFlowController from './src/markets/willhaben/willhabenFlowController.js';
-import BazosFlowController from './src/markets/bazos/bazosFlowController.js';
-import { getMarketList } from './src/utils.js';
-import { io } from "socket.io-client";
+import {io} from "socket.io-client";
 
 class BotController {
     constructor(token) {
@@ -27,14 +14,8 @@ class BotController {
         this.commandList = [
             { command: '/start', description: 'start bot' }
         ];
-        this.selectedMarketFlow = null;
-        this.linkData = {};
-        this.selectedMarket = '';
-        this.selectedCountry = '';
-        this.trc20WalletDataAwait = false;
         this.notificationMessageAwait = false;
-        this.userData = {};
-        this.MARKET_LIST = [];
+        this.currencyAwait = false;
         this.onMessageHandler = this.onMessageHandler.bind(this);
     }
 
@@ -46,24 +27,14 @@ class BotController {
 
     startWork(msg) {
         this.clean();
-        this.addEventListeners()
-        const chatId = msg.chat.id;
-        this.socket = io(SERVER_URL);
-        this.authUser(msg).then(data => {
-            if (data.type == "whaitlist") {
-                UIManager.whaitListResponce(chatId);
-            }
-            else if (data.type === USER_ROLES.ADMIN || data.type === USER_ROLES.WORKER || data.type === USER_ROLES.VBIVER) {
-                this.socket.emit('new-user', {chatId: chatId.toString()});
-                UIManager.mainMenu(chatId, this.userData, data.type);
-            }
-            else if (data.type === USER_ROLES.REJECTED) {
-                this.bot.sendMessage(chatId, 'Вашу заявку не приняли');
-            }
-            else {
-                this.initNewUserFlow(msg.chat);
-            }
-        });
+        this.addEventListeners();
+        const chatData = msg.chat;
+        const chatId = chatData.id;
+        // this.socket = io(SERVER_URL);
+        // this.authUser(msg).then(data => {
+        //         this.socket.emit('new-user', {chatId: chatId.toString()});
+                UIManager.adminMainMenu(chatId, chatData);
+        // });
     }
 
     async authUser(msg) {
@@ -83,42 +54,6 @@ class BotController {
         return this.userData;
     }
 
-    initNewUserFlow(chat) {
-        const chatId = chat.id;
-        UIManager.newUser(chatId, chat);
-    }
-
-    async newUserRequest(chatData) {
-        const adminsData = await fetch(SERVER_URL + '/askForNewUser', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({...chatData, teamName: BOT_TEAM_NAME})
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            return data
-        })
-        adminsData.forEach(element => {
-            UIManager.adminAskForApprove(element.id, chatData);
-        });
-    }
-
-    async approveNewUser(id, type) {
-        const userData = await fetch(SERVER_URL + '/approveNewUser', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({id, type, teamName: BOT_TEAM_NAME})
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            return data
-        })
-        UIManager.approveMessage(id, userData.type)
-    }
 
     addEventListeners() {
         this.bot.on('message', this.onMessageHandler);
@@ -126,124 +61,16 @@ class BotController {
         this.bot.on('callback_query', query => {
             const chatId = query.message.chat.id;
             if (typeof (query.data) === "string") {
-                let dataParam = null;
-                if (query.data.includes(ADMINS_CHOOSE.ADMINS_NEW_WORKER)) {
-                    let separateChatId = [...query.data.matchAll(/(ADMINS_NEW_WORKER)(.*)/gm)];
-                    query.data = separateChatId[0][1];
-                    dataParam = separateChatId[0][2];
-                }
-                if (query.data.includes(ADMINS_CHOOSE.ADMINS_NEW_ADMIN)) {
-                    let separateChatId = [...query.data.matchAll(/(ADMINS_NEW_ADMIN)(.*)/gm)];
-                    query.data = separateChatId[0][1];
-                    dataParam = separateChatId[0][2];
-                }
-                if (query.data.includes(MAIN_MENU_UI_CONTROLS_EVENT.SELECTED_LINK)) {
-                    let separateChatId = [...query.data.matchAll(/(SELECTED_LINK)(.*)/gm)];
-                    query.data = separateChatId[0][1];
-                    dataParam = separateChatId[0][2];
-                }
-                if (query.data.includes(MAIN_MENU_UI_CONTROLS_EVENT.REMOVE_LINK)) {
-                    let separateChatId = [...query.data.matchAll(/(REMOVE_LINK)(.*)/gm)];
-                    query.data = separateChatId[0][1];
-                    dataParam = separateChatId[0][2];
-                }
                 switch (query.data) {
-                    case NEW_USER_REPLY.YES:
-                        this.newUserRequest(query.message.chat);
-                        UIManager.newUserAccept(chatId);
-                        break;
-                    case NEW_USER_REPLY.NO:
-                        UIManager.newUserDecline(chatId);
-                        break;   
-                    case REQUEST_UI_CONTROL_EVENTS.CHANGE_NAME:
-                        this.selectedMarketFlow.buyerDataEditNameAwait = true;
-                        UIManager.requestDataEdit(chatId, query.data)
-                        break;
-                    case REQUEST_UI_CONTROL_EVENTS.CHANGE_PRICE:
-                        this.selectedMarketFlow.buyerDataEditPriceAwait = true;
-                        UIManager.requestDataEdit(chatId, query.data)
-                        break;
-                    case REQUEST_UI_CONTROL_EVENTS.LINK1:
-                        break;
-                    case REQUEST_UI_CONTROL_EVENTS.LINK2:
-                        break;
-                    case REQUEST_UI_CONTROL_EVENTS.MY_LINKS:
-                        this.requestMyLinks(chatId);
-                        break;
-                    case REQUEST_UI_CONTROL_EVENTS.NOTIFICATION:
+                    case MAIN_MENU_UI_CONTROLS_EVENT.NOTIFICATION:
                         this.notificationMessageAwait = true;
                         UIManager.notifyMessageAwait(chatId)
                         break;
-                    case REQUEST_UI_CONTROL_EVENTS.MAIN_MENU:
-                        UIManager.mainMenu(chatId, this.userData, this.userData.type);
+                    case MAIN_MENU_UI_CONTROLS_EVENT.SET_CURRENCY_VALUE:
+                        UIManager.setCurrencyValueUI(chatId)
                         break;
-                    case MAIN_MENU_UI_CONTROLS_EVENT.CREATE_LINK:
-                        UIManager.countrySelector(chatId);
-                        break;
-                    case MAIN_MENU_UI_CONTROLS_EVENT.SETTINGS:
-                        UIManager.settingsMenu(chatId, this.userData);
-                        break;
-                    case MAIN_MENU_UI_CONTROLS_EVENT.SELECTED_LINK:
-                        const linkData = this.userData.links[dataParam];
-                        this.selectedCountry = linkData.selectedCountry;
-                        this.selectedMarket = linkData.selectedMarket
-                        this.selectMarketFlow(linkData.selectedMarket, chatId, linkData);
-                        this.selectedMarketFlow.selectedCountry = linkData.selectedCountry;
-                        this.selectedMarketFlow.selectedMarket = linkData.selectedMarket;
-                        this.selectedMarketFlow.productData = {
-                            name: linkData.staffName,
-                            price: linkData.price,
-                            image: linkData.staffImgURL
-                        }
-                        this.selectedMarketFlow.buyerName = linkData.buyerName;
-                        this.selectedMarketFlow.buyerLocation = linkData.buyerLocation;
-                        this.selectedMarketFlow.generateMarketControls(chatId, linkData)
-                        break;
-                    case MAIN_MENU_UI_CONTROLS_EVENT.REMOVE_LINK:
-                        const linkID = dataParam;
-                        this.removeLinkFromAccount(chatId, linkID);
-                        break;
-                    case MAIN_MENU_UI_CONTROLS_EVENT.USERS_LIST:
-                        this.getUsersList().then(usersList => {
-                            console.log(usersList);
-                            UIManager.usersListUI(chatId, usersList);
-                        });
-                        break;
-                    case MAIN_MENU_UI_CONTROLS_EVENT.WORKERS_LIST:
-                        this.getRoleList(USER_ROLES.WORKER).then(workersList => {
-                            UIManager.workersListUI(chatId, workersList);
-                        });
-                        break;
-                    case MAIN_MENU_UI_CONTROLS_EVENT.VBIVERS_LIST:
-                        this.getRoleList(USER_ROLES.VBIVER).then(workersList => {
-                            UIManager.vbiversListUI(chatId, workersList);
-                        });
-                        break;
-                    case SEQUE_SCREEN_OPTIONS.PASS_3D:
-                        console.log(SEQUE_SCREEN_OPTIONS.PASS_3D, chatId, this.socket);
-                        this.socket.emit(SEQUE_SCREEN_OPTIONS.PASS_3D, chatId)
-                        break;
-                    case ADMINS_CHOOSE.ADMINS_NEW_ADMIN:
-                        this.approveNewUser(dataParam, USER_ROLES.ADMIN);
-                        break;
-                    case ADMINS_CHOOSE.ADMINS_NEW_WORKER_REJECT:
-                        this.approveNewUser(dataParam, USER_ROLES.REJECTED);
-                        break;
-                    case SETTINGS_EVENTS.LINK_NEW_TRC_20:
-                        UIManager.requestTRC20Wallet(chatId);
-                        this.trc20WalletDataAwait = true;
-                        break;
-                    case ADMINS_CHOOSE.ADMINS_NEW_WORKER:
-                        this.approveNewUser(dataParam, USER_ROLES.WORKER);
-                        break; 
-                    case ADMINS_CHOOSE.ADMINS_NEW_VBIVER:
-                        this.approveNewUser(dataParam, USER_ROLES.VBIVER);
-                        break; 
-                    case this.MARKET_LIST.find((el) => query.data === el):
-                        this.marketSelectedHandler(query);
-                        break;
-                    case COUNTRIES_NAMES.find((el) => query.data === el):
-                        this.countrySelectedHandler(query)
+                    case SET_CURRENCY_EVENT[query.data]:
+                        this.currencyAwait = true;
                         break;
                     default:
                         this.bot.sendMessage(chatId, "Выберите корректную кнопку");
@@ -260,53 +87,25 @@ class BotController {
         if (text === "/start") {
             return this.startWork(msg);
         }
-        if (msg.reply_to_message && this.selectedMarketFlow) {
-            this.socket.emit('send-telega-message', msg.text);
-            return true;
-        }
-
-        if (this.trc20WalletDataAwait) {
-            this.updateTRC20WalletData(text);
-            return true;
-        }
-
-        if (this.notificationMessageAwait) {
-            this.notificationMessageAwait = false;
-            this.notifyUsers(text);
-            return true;
-        }
-
-        if (this.selectedMarketFlow) {
-            this.selectedMarketFlow.onMessage(text || photo, chatId)
-            return true;
+        if (this.currencyAwait) {
+            this.setCurrencyValue(chatId, text)
         }
 
         this.bot.sendMessage(chatId, 'I don`t understand you motherfucker');
     }
 
-    async getUsersList() {
-        const usersList = await fetch(SERVER_URL + `/usersList?teamName=${BOT_TEAM_NAME}`)
-        .then(response => {
-            return response.json()
-        }).then(data => {
-            return data;
+    async setCurrencyValue(chatId, value) {
+        const currencyResponce = await fetch(`${SERVER_URL}/currencyValue`, {
+            method: "POST",
+            body: JSON.stringify({value})
+        }).then(response => {
+            return response.json();
+        }).then(moreResponsiveResponse => {
+            return moreResponsiveResponse;
         })
-        return usersList;
+        this.bot.sendMessage(chatId, currencyResponce.text)
     }
 
-    async getRoleList(role) {
-        const roleList = await this.getUsersList().then(roleList => {
-            const roleListArray = [];
-            for (const key in roleList) {
-                const el = roleList[key];
-                if (el.type === role) {
-                    roleListArray.push(el);
-                }
-            }
-            return roleListArray;
-        })
-        return roleList;
-    }
 
     async notifyUsers(text) {
         this.getUsersList().then(usersList => {
@@ -317,108 +116,9 @@ class BotController {
         })
     }
 
-    onRemoveButtonPressed(chatId) {
-        UIManager.onRemoveButtonHandled(chatId);
-    }
-
-    onBackToMainMenuHandled(chatId) {
-        this.startWork(chatId);
-    }
-
-    async updateTRC20WalletData(walletData) {
-        const userData = await fetch(SERVER_URL + '/updateTRC20Wallet', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({id: this.userData.id, walletData, teamName: BOT_TEAM_NAME})
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            return data;
-        })
-        this.userData = userData;
-        UIManager.updateDataCompleate(this.userData.id);
-    }
-
-    async requestMyLinks(id) {
-        const myLinks = await fetch(SERVER_URL + '/myLinks', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({id, teamName: BOT_TEAM_NAME})
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            return data;
-        })
-        this.userData.links = myLinks;
-        UIManager.linksList(id, myLinks);
-    }
-
-    marketSelectedHandler(callBackData) {
-        const chatId = callBackData.message.chat.id;
-        const marketInputName = callBackData.data;
-        this.MARKET_LIST.forEach(element => {
-            if (element === marketInputName) {
-                this.selectMarketFlow(element, chatId);
-                this.selectedMarketFlow.start();
-            }
-        });
-
-    }
-
-    async removeLinkFromAccount(chatId, linkId) {
-        await fetch(SERVER_URL + '/myLinks', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({userId: chatId, linkId, teamName: BOT_TEAM_NAME})
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            this.requestMyLinks(chatId);
-            this.onRemoveButtonPressed(chatId);
-        })
-    }
-
-    selectMarketFlow(marketName, chatId, linkData) {
-        this.selectedMarket = marketName;
-        switch (marketName) {
-            case MARKET_NAMES.OLX:
-                this.selectedMarketFlow = new OLXFlowController(this.bot, chatId, this.selectedMarket, this.selectedCountry, linkData);
-                break;
-            case MARKET_NAMES.WILLHABEN:
-                this.selectedMarketFlow = new WillhabenFlowController(this.bot, chatId, this.selectedMarket, this.selectedCountry, linkData);
-                break;
-            case MARKET_NAMES.BAZOS:
-                this.selectedMarketFlow = new BazosFlowController(this.bot, chatId, this.selectedMarket, this.selectedCountry, linkData);
-                break;
-            default:
-                this.bot.sendMessage(chatId, "маркет не найден");
-        }
-    }
-
-    countrySelectedHandler(callBackData) {
-        const chatId = callBackData.message.chat.id;
-        const countryInputName = callBackData.data;
-        this.MARKET_LIST = getMarketList(countryInputName);
-        COUNTRIES_NAMES.forEach(element => {
-            if (element === countryInputName) {
-                this.selectedCountry = element;
-                UIManager.marketSelector(chatId, this.selectedCountry, this.MARKET_LIST);
-            }
-        });
-    }
 
     clean() {
-        this.selectedCountry = '';
-        this.selectedMarket = '';
-        this.selectedMarketFlow = null;
         this.bot.removeAllListeners();
-        this.MARKET_LIST = [];
     }
 
 }
--- src/UIManager.js
+++ src/UIManager.js
@@ -1,14 +1,37 @@
-import { EMOJI_NAMES, MAIN_MENU_UI_CONTROLS_EVENT } from './constants.js';
+import {EMOJI_NAMES, MAIN_MENU_UI_CONTROLS_EVENT, SET_CURRENCY_EVENT} from './constants.js';
 import { getEmoji } from './utils.js';
 class UIManager {
     constructor() {
         this.bot = null;
-        this.mainMenuUIOptions = {
+        this.userMainMenuUI = {
             reply_markup: {
                 inline_keyboard: [
                     [{ text: `${getEmoji(EMOJI_NAMES.CREATE_LINK)} Замовити обмін`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.CREATE_EXCHANGE_REQUEST }],
                     [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} Зв'язок з представником`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.CHAT }],
                     [{ text: `${getEmoji(EMOJI_NAMES.SETTINGS)} Про бот`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.BOT_INFO }],
+                ]
+            }
+        }
+        this.adminMainMenuUI = {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: `${getEmoji(EMOJI_NAMES.CREATE_LINK)} Виставити курс`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.SET_CURRENCY_VALUE }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} Переглянути останні транзакції`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.CHAT }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} Переглянути транзакцію`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.CHAT }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} Зробити оголошення`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.NOTIFICATION }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.SETTINGS)} Про бот`, callback_data: MAIN_MENU_UI_CONTROLS_EVENT.BOT_INFO }],
+                ]
+            }
+        }
+        this.currencylistUI = {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} USDT: `, callback_data: SET_CURRENCY_EVENT.USDT }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} USDS: `, callback_data: SET_CURRENCY_EVENT.USDS }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} EUR: `, callback_data: SET_CURRENCY_EVENT.EUR }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} USD: `, callback_data: SET_CURRENCY_EVENT.USD }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} BTC: `, callback_data: SET_CURRENCY_EVENT.BTC }],
+                    [{ text: `${getEmoji(EMOJI_NAMES.MY_LINKS)} ETH: `, callback_data: SET_CURRENCY_EVENT.ETH }],
                 ]
             }
         }
@@ -18,8 +41,12 @@ class UIManager {
         this.bot = bot;
     }
 
-    mainMenu(chatId, data) {
-        this.bot.sendMessage(chatId, `Привіт ${data.first_name || data.last_name}  \nГоловне меню:`, this.mainMenuUIOptions);
+    userMainMenu(chatId, data) {
+        this.bot.sendMessage(chatId, `Привіт ${data.first_name || data.last_name}  \nГоловне меню:`, this.userMainMenuUI);
+    }
+
+    adminMainMenu(chatId, data) {
+        this.bot.sendMessage(chatId, `Привіт ${data.first_name || data.last_name}  \nГоловне меню:`, this.adminMainMenuUI);
     }
 
     botInfo(chatId) {
@@ -28,6 +55,10 @@ class UIManager {
     
     currencylist(chatId) {
         this.bot.sendMessage(chatId, `USDT: 3213123 \nBTC: 213 \nETH: 21315 \n`);
+    }
+
+    setCurrencyValueUI(chatId) {
+        this.bot.sendMessage(chatId, `Оберіть валюту`, this.currencylistUI);
     }
 
     siteLink(chatId) {
@@ -40,6 +71,10 @@ class UIManager {
 
     chatWithManager(chatId) {
         this.bot.sendMessage(chatId, `Очікуйте відповіді від представика`);
+    }
+
+    notifyMessageAwait(chatId) {
+        this.bot.sendMessage(chatId, `Зробіть оголошення`);
     }
 }
 
